28aac6ea0e7ac215198a6b4d97f01cebbb036894
Kaumini Perera <147579249+Kaumini-Perera@users.noreply.github.com>
2023-10-16 00:15:28 +0530

    Update aabidh's repo.py

--- aabidh.py
+++ aabidh.py
@@ -1,31 +1,19 @@
 class Graph:
-    """
-    A class representing an arbitrary sized graph.
-    """
     def __init__(self, size):
         self.matrix = [[0] * size for _ in range(size)]
         self.size = size
 
     def add_edge(self, u, v):
-        """
-        Add an edge between vertices u and v.
-        """
         if 0 <= u < self.size and 0 <= v < self.size:
             self.matrix[u][v] = 1
             self.matrix[v][u] = 1
 
     def is_edge(self, u, v):
-        """
-        Check if there is an edge between vertices u and v.
-        """
         if 0 <= u < self.size and 0 <= v < self.size:
             return self.matrix[u][v] == 1
         return False
 
     def bfs(self, start_vertex):
-        """
-        Perform breadth-first search starting from the given vertex.
-        """
         visited = [False] * self.size
         queue = [start_vertex]
         visited[start_vertex] = True
@@ -40,16 +28,10 @@ class Graph:
                     visited[i] = True
 
     def dfs(self, start_vertex):
-        """
-        Perform depth-first search starting from the given vertex.
-        """
         visited = [False] * self.size
         self._dfs_recursive(start_vertex, visited)
 
     def _dfs_recursive(self, vertex, visited):
-        """
-        Helper function for recursive depth-first search.
-        """
         visited[vertex] = True
         print(vertex, end=' ')
 
@@ -58,10 +40,8 @@ class Graph:
                 self._dfs_recursive(i, visited)
 
 
-# Example Usage:
-graph = Graph(11)  # Change the size to 11
+graph = Graph(11)
 
-# Adding edges
 graph.add_edge(0, 1)
 graph.add_edge(0, 2)
 graph.add_edge(0, 3)
